911cc7a83392ebc4be6825cae26b3633eee6df91
kcooks614 <kcc73@cornell.edu>
2022-11-13 14:40:19 -0500

    Create config.js

---
+++ Lab12/config.js
@@ -0,0 +1,114 @@
+var config = {
+    style:  'mapbox://styles/iamwfx/cjjx6bl926vgt2ss0l9dbpmxz',
+    accessToken: 'pk.eyJ1Ijoia2Nvb2tzNjE0IiwiYSI6ImNsOHlqejA0dTBoOG0zb3F2dDFtMTJ0cngifQ.f02Y4QD9JCis65byZ2OEqw',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    theme: 'light',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    title: 'Coastal Flooding in New York City',
+    subtitle: 'Assessing Risks to Vulnerable Housing',
+    byline: 'By Kellen Cooks',
+    para1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor lacus a tortor maximus tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec non laoreet libero. Etiam elementum ante at diam fringilla fermentum. Nunc ultricies eleifend nunc ut fermentum. Sed sed tortor ut urna fringilla porttitor. Proin imperdiet felis at fermentum consectetur. Vestibulum vel faucibus turpis. Aenean tellus neque, auctor hendrerit augue eleifend, varius tincidunt quam. Cras varius, augue in congue elementum, orci felis ornare risus, quis sollicitudin nisl enim at felis.',
+    para2: 'Quisque convallis dolor non dignissim varius. Curabitur tincidunt iaculis odio in pretium. Vestibulum molestie orci lorem, sed accumsan purus aliquam ut. Nullam nisl lectus, laoreet id diam ut, facilisis vehicula urna. Aenean facilisis tristique facilisis. Sed ultrices faucibus justo, quis tempor nunc interdum facilisis. Ut ut urna libero. Duis vehicula neque viverra, eleifend mi at, elementum sem. Vivamus dolor tortor, vulputate nec tellus vitae, blandit molestie eros. Duis rhoncus fringilla ex, in dapibus nisl sodales sed. Sed efficitur venenatis sapien, eu imperdiet diam congue id.',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'first-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'Display Title',
+            image: 'images/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [-122.418398, 37.759483],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-chapter',
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: 'images/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+    id: 'interlude',
+    alignment: 'center',
+    hidden: false,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies tempor purus, eget tempus sem bibendum vel. Maecenas at justo non felis accumsan condimentum vitae et leo. Sed sodales nec risus maximus congue. Ut quis tincidunt leo. Pellentesque sapien nisl, congue tempus mauris in, pulvinar mattis turpis. Nullam sit amet justo egestas, laoreet mauris ut, pulvinar ipsum. Pellentesque vehicula imperdiet iaculis. Nam in tellus metus. Donec a est non orci efficitur ornare id mattis eros. Aliquam vel facilisis ligula. Nullam tincidunt, sapien vitae bibendum consequat, nibh eros eleifend ligula, et aliquam enim justo eget felis. Sed nec vehicula metus. Pellentesque rutrum dui quis egestas hendrerit. Nullam auctor et tortor sit amet congue.',
+    video: 'Images/902-1_902-2364-PD2_preview.mp4',
+    location: {
+      center: [-73.97615, 40.71901],
+      zoom: 14.84,
+      pitch: 48,
+      bearing: 142.44,
+    },
+},
+        {
+            id: 'third-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: 'Third Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+              center: [-73.97615, 40.71901],
+              zoom: 15.84,
+              pitch: 56,
+              bearing: 260.44,
+              speed: .5, // make the flying slow
+              curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'housing',
+                    opacity: 0,
+                    duration: 10
+                }],
+            onChapterExit: []
+        }
+    ]
+};
